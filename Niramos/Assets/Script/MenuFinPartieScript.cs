42fcaef5b9792503f7b365a2e85ebbed549f370a
Unknown <guillaume.d.marceau@gmail.com>
2019-01-29 16:34:25 -0500

    ajouter vue fin partie

---
+++ Niramos/Assets/Script/MenuFinPartieScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuFinPartieScript : MonoBehaviour
+{
+    [SerializeField]
+    private Button bouttonRejouer;
+    [SerializeField]
+    private Button bouttonRetourMenu;
+    [SerializeField]
+    private Image imageGagant;
+    [SerializeField]
+    private Text texteFinPartie;
+
+    private void OnEnable()
+    {
+        //imageGagant.sprite; = sprite gagnant
+        //texteFinPartie.text = text gagnant
+        bouttonRejouer.onClick.AddListener(rejouer);
+        bouttonRetourMenu.onClick.AddListener(retourMenu);
+    }
+
+    private void rejouer()
+    {
+        //TODO: action rejouer
+    }
+    private void retourMenu()
+    {
+        //TODO: charger sceneMenuPrincipale
+    }
+}
