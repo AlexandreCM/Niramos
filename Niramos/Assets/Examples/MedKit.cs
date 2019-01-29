d88541b0d6eda30e2680d4a41af672c35deeeea0
Antoine Beauvais <1901037@cgmatane.qc.ca>
2019-01-29 15:39:03 -0500

    Created GameScene scene and added example scripts.

---
+++ Niramos/Assets/Examples/MedKit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedKit : MonoBehaviour {
+
+    public float HealAmount = 30;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Player t_Player = collision.gameObject.GetComponent<Player>();
+
+        if (t_Player)
+        {
+            t_Player.Heal(HealAmount);
+
+            Destroy(gameObject);
+        }
+    }
+
+   
+}
---
+++ Niramos/Assets/Examples/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float Speed = 1;
+    public float Hp;
+    private float m_MaxHp = 100;
+
+	// Use this for initialization
+	void Start () {
+        Hp = 30;
+        EventManager.TriggerEvent("HealthChanged");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position += transform.forward * Speed * Time.deltaTime;
+	}
+
+    public void Heal(float a_Heal)
+    {
+        Hp += a_Heal;
+
+        if (Hp > m_MaxHp)
+        {
+            Hp = m_MaxHp;
+        }
+
+        EventManager.TriggerEvent("HealthChanged");
+    }
+
+    public void TakeDamage(float a_Damage)
+    {
+        Hp -= a_Damage;
+
+        if (Hp < 0)
+        {
+            Hp = 0;
+            Die();
+        }
+
+        EventManager.TriggerEvent("HealthChanged");
+    }
+
+    private void Die()
+    {
+        // TODO
+    }
+}
