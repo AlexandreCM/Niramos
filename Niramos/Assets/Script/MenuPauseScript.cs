43f3aa51366ec61d62caf1c2949504e37c12c9c5
Unknown <guillaume.d.marceau@gmail.com>
2019-01-29 16:01:06 -0500

    commit menu pause

---
+++ Niramos/Assets/Script/MenuPauseScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPauseScript : MonoBehaviour
+{
+    [SerializeField]
+    private Button bouttonReprendre;
+    [SerializeField]
+    private Button bouttonRetourMenu;
+
+    private void OnEnable()
+    {
+        bouttonReprendre.onClick.AddListener(reprendre);
+        bouttonRetourMenu.onClick.AddListener(retourMenu);
+    }
+
+    private void reprendre()
+    {
+        //TODO: action reprendre
+    }
+    private void retourMenu()
+    {
+        //TODO: charger sceneMenuPrincipale
+    }
+}
