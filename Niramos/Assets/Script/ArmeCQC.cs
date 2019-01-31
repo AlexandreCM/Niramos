0526b20fea57dab0bbf31446402c4dd66250b49b
Unknown <guillaume.d.marceau@gmail.com>
2019-01-31 10:48:15 -0500

    debut des arme et degat

---
+++ Niramos/Assets/Script/ArmeCQC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmeCQC : MonoBehaviour
+{
+    private bool estPickup = true;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision");
+        if (collision.gameObject.layer == this.gameObject.layer)
+        {
+            GameObject Joueur = collision.gameObject;
+            this.GetComponents<CapsuleCollider2D>()[0].enabled = false;
+            this.GetComponents<CapsuleCollider2D>()[1].enabled = false;
+            this.transform.parent = Joueur.transform;
+            this.GetComponent<Rigidbody2D>().isKinematic = true;
+            this.gameObject.transform.position.Set(5,0,0);
+            Transform transformParent = this.transform.parent.transform;
+            this.gameObject.transform.rotation.Set(transformParent.rotation.x, transformParent.rotation.y, transformParent.rotation.z, transformParent.rotation.w);
+        }
+    }
+}
---
+++ Niramos/Assets/Script/VieJoueur.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VieJoueur : MonoBehaviour
+{
+    public float vie = 100;
+    public float vieMax = 100;
+
+    public void reduireVie(int quantiter)
+    {
+        vie -= quantiter;
+        GestionnaireEvenement.declancherEvenement("vieChanger");
+        if (vie <= 0)
+        {
+            GestionnaireEvenement.declancherEvenement("joueurMort");
+        }
+        Debug.Log(vie);
+    }
+}
