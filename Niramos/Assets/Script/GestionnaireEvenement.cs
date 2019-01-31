9790e1ae498edfd2cbe361f88f935a51c4b6c05f
Unknown <guillaume.d.marceau@gmail.com>
2019-01-31 09:06:49 -0500

    script  gestionnaire evenement

---
+++ Niramos/Assets/Script/GestionnaireEvenement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public static class GestionnaireEvenement
+{
+    private static Dictionary<string, UnityEvent> dictionnaireEvenement;
+
+    public static void ajouterEvenement(string nomEvenement, UnityAction action)
+    {
+        UnityEvent evenement;
+        dictionnaireEvenement.TryGetValue(nomEvenement, out evenement);
+        if (evenement == null)
+        {
+            dictionnaireEvenement.Add(nomEvenement, evenement);
+        }
+
+        evenement.AddListener(action);
+    }
+
+    public static void retirerEvenement(string nomEvenement, UnityAction action)
+    {
+        UnityEvent evenement;
+        dictionnaireEvenement.TryGetValue(nomEvenement, out evenement);
+        if (evenement != null)
+        {
+            evenement.RemoveListener(action);
+        }
+    }
+
+    public static void declancherEvenement(string nomEvenement)
+    {
+        UnityEvent evenement;
+        dictionnaireEvenement.TryGetValue(nomEvenement, out evenement);
+        if (evenement != null)
+        {
+            evenement.Invoke();
+        }
+        else
+        {
+            Debug.LogError(nomEvenement + " existe pas");
+        }
+    }
+}
