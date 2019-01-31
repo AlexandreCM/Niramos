1112a0780953ca3b517e538a32003d97b7162a1c
Antoine Beauvais <1901037@cgmatane.qc.ca>
2019-01-31 08:12:40 -0500

    Création de la scène FinPartieScene et écriture du script pour le HUD.
    Les fichiers restent à terminer.

---
+++ Niramos/Assets/Script/UI_Time.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Time : MonoBehaviour
+{
+
+    private float temps;
+    private float initialTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
