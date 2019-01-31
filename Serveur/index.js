be1aada73884ba8b0d9f0dc9c0dba2d31b4d6545
Arthur <arthur.cheramy@gmail.com>
2019-01-31 10:36:25 -0500

    Architecture serveur complète

--- Serveur/index.js
+++ Serveur/index.js
@@ -8,17 +8,46 @@ app.set('port', process.env.PORT || 3000);
 
 var clients = [];
 
+//io est une variable de socket.io regroupant des évènements
 io.on("connection", function (socket) {
-    var currentUser;
+    var joueurCourant;
     socket.on("USER_CONNECT", function () {
         console.log('Un joueur vient de se connecter');
         for (var i = 0; i < clients.length; i++) {
-            socket.emit("USER_CONNECTED", { name: clients[i].name, position: clients[i].position });
+            socket.emit("USER_CONNECTED", { nom: clients[i].nom, position: clients[i].position });
         }
-        //Arrêt ici
-        console.log("Joueur : "+clients[i].name)
+        console.log("Joueur : " + clients[i].nom + " est connecté.")
     });
+
+    socket.on("PLAY", function (data) {
+        joueurCourant = {
+            nom: data.nom,
+            position: data.position
+        }
+
+        clients.push(joueurCourant);
+        socket.emit("PLAY", joueurCourant);
+        socket.broadcast.emit("USER_CONNECTED", joueurCourant);
+    });
+
+    socket.on("MOVE", function (data) {
+        joueurCourant.position = data.position;
+        socket.emit("MOVE", joueurCourant);
+        socket.broadcast.emit("MOVE", joueurCourant);
+        console.log(joueurCourant.nom + "se déplace vers " + joueurCourant.position);
+    })
+
+    socket.on("disconnect", function () {
+        socket.broadcast.emit("USER_DISCONNECTED", joueurCourant);
+        for (var i = 0; i < clients.length; i++) {
+            if (clients[i].nom == joueurCourant.nom) {
+                console.log("Joueur " + clients[i].nom + " s'est déconnecté.");
+                clients.splice(i, 1);
+            }
+        }
+    })
 });
+
 
 
 server.listen(app.get('port'), function () {
