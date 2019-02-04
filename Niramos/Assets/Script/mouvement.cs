3e909a5340e70d1a0e47c74a2a8dd0f994a07d62
AlexandreCM <alexandre.mezeray@gmail.com>
2019-02-04 09:59:28 -0500

    Control du personnage
    droite gauche, saut, atterissage forcer

---
+++ Niramos/Assets/Script/mouvement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouvement : MonoBehaviour
+{
+    public float vitesse = 5;
+
+    public int forceSaut = 5;
+    public bool auSol;
+    public LayerMask terrain;
+
+    public int forceFracasse = -10;
+    bool stickDownLast;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // au sol ?
+        auSol = Physics2D.OverlapArea(
+            new Vector2(transform.position.x -0.5f, transform.position.y - 0.5f), 
+            new Vector2(transform.position.x, transform.position.y), 
+            terrain
+        );
+
+        // Mouvement du joueur
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0);
+        transform.position += move * vitesse * Time.deltaTime;
+        
+
+        if (Input.GetButtonDown("Jump") && auSol)  //makes player jump
+
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forceSaut), ForceMode2D.Impulse);
+            return;
+        }
+
+        // Attaque vers le bas
+        if(Input.GetAxis("Vertical") < 0) {
+            if(!stickDownLast) {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forceFracasse), ForceMode2D.Impulse);
+            }
+            stickDownLast = true;
+        } 
+        else {
+            stickDownLast = false; 
+        }
+    }
+}
