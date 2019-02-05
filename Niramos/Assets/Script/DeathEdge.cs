6a1aeb62120fc9bfeb645fbafbe0676f6b92c056
Antoine Beauvais <1901037@cgmatane.qc.ca>
2019-02-05 15:53:57 -0500

    Création d'un système de réapparition en cas de sortie.
    Système très simple de réapparition instantané.

---
+++ Niramos/Assets/Script/DeathEdge.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathEdge : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Dead.");
+        //Detecting the Grid Position of Player
+        if (collision.gameObject.name == "TestPlayer")
+        {        
+            collision.gameObject.transform.position = new Vector3(-1.38f, 0.0f, 0.0f);
+        }
+ 
+    }
+}
