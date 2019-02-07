daed22469d295c5ee63c67f9f06887d0c6a3c74c
Antoine Beauvais <1901037@cgmatane.qc.ca>
2019-02-07 08:43:22 -0500

    Création de scripts pour les dangers environnementaux.
    Il est désormais possible de placer des objets environnementaux qui pourront blesser les joueurs lorsqu'ils rentrent en collision avec l'obstacle.

---
+++ Niramos/Assets/Script/ObstacleEnvironnemental.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleEnvironnemental : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay2D(Collision2D collision) {
+        VieJoueur vieJ = collision.gameObject.GetComponent<VieJoueur>();
+        if (vieJ) {
+            vieJ.faireDegat(1.0f);
+        }
+    }
+}
