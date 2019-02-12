e68e2cf705710aacb3c1f8627dec6c28f9c0b92f
Unknown <guillaume.d.marceau@gmail.com>
2019-02-12 16:12:18 -0500

    terminer arc et arme cqc

---
+++ Niramos/Assets/Script/ManagerJoueur.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerJoueur : MonoBehaviour
+{
+    public GameObject objetEnMain = null;
+    private bool directionVerDroite = true;
+
+    private void OnEnable()
+    {
+        if(this.gameObject.GetComponent<mouvement>().enabled)
+            GestionnaireEvenement.ajouterEvenement("directionChanger", changerDirection);
+    }
+
+    public bool getDirectionVerDroite()
+    {
+        return directionVerDroite;
+    }
+
+    public void setDirectionVerDroite(bool direction)
+    {
+        if(directionVerDroite != direction)
+        {
+            directionVerDroite = direction;
+            changerDirectionArme();
+        }
+    }
+
+    private void changerDirection()
+    {
+        directionVerDroite = !directionVerDroite;
+        changerDirectionArme();
+    }
+
+    private void changerDirectionArme()
+    {
+        ArmeCQC arme = null;
+        arme = objetEnMain.GetComponent<ArmeCQC>();
+        Arc arc = objetEnMain.GetComponent<Arc>();
+
+        if (arc != null) arc.changerDirection();
+        if (arme != null) arme.changerDirection();
+    }
+}
