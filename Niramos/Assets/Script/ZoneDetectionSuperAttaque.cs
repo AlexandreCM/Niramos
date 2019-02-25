294c34fe87e1f0ca16e4c29c2948779737a38e54
AlexandreCM <alexandre.mezeray@gmail.com>
2019-02-25 09:51:30 -0500

    detection de joueur pour une super attaque a corriger

---
+++ Niramos/Assets/Script/ZoneDetectionSuperAttaque.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneDetectionSuperAttaque : MonoBehaviour
+{
+    public List<GameObject> listeJoueur;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Player") return;
+
+        //We know we just hit a player
+        //add collision player to a list
+        if (collision.gameObject.tag == "Player")
+        {
+            listeJoueur.Add(collision.gameObject);
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Player") return;
+
+        //We know we just hit a player
+        //remove collision player from list
+        if (collision.gameObject.tag == "Player")
+        {
+            listeJoueur.Remove(collision.gameObject);
+        }
+
+    }
+}
