5c8f848dd59ba9812a054e1f843b6e730372ce08
FoxMcCloud45 <foxmccloud45@gmail.com>
2019-03-04 09:32:01 -0500

    Les obstacles environnementaux fonctionnent correctement avec Rogue_01.
    Les objets Rogue_01 interagissent désormais correctement lors de la réception de dégâts.

--- Niramos/Assets/Script/ObstacleEnvironnemental.cs
+++ Niramos/Assets/Script/ObstacleEnvironnemental.cs
@@ -19,12 +19,12 @@ public class ObstacleEnvironnemental : MonoBehaviour
         
     }
 
-    void OnCollisionEnter2D(Collision2D collision) {
+    void OnTriggerEnter2D(Collider2D collider) {
         Debug.Log("INFO    ObstacleEnvironnemental:OnCollisionEnter2D(Collision2D collision) triggered for " + this.gameObject.name);
-        VieJoueur vieJ = collision.gameObject.GetComponent<VieJoueur>();
-        DegatsJoueur testJ = collision.gameObject.GetComponent<DegatsJoueur>();
+        VieJoueur vieJ = collider.gameObject.GetComponent<VieJoueur>();
+        DegatsJoueur testJ = collider.gameObject.GetComponent<DegatsJoueur>();
         if (vieJ && testJ) {
-            Vector3 force3d = collision.gameObject.transform.position - this.gameObject.transform.position;
+            Vector3 force3d = collider.gameObject.transform.position - this.gameObject.transform.position;
             Vector2 force2d = new Vector2(force3d.x, force3d.y);
             testJ.repousserJoueur(force2d, this.dmg * 32.0f);
             vieJ.faireDegat(this.dmg);
