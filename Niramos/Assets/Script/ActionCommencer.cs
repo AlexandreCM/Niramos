b46b1dd4233a7c7e2cdd2c1e7b4dfa7341e1c6bd
AlexandreCM <alexandre.mezeray@gmail.com>
2019-03-14 09:06:14 -0400

    Deplacement Script ActionCommencer dans dossier script

---
+++ Niramos/Assets/Script/ActionCommencer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionCommencer : MonoBehaviour
+{
+    Button myButton;
+
+    void Awake()
+    {
+        myButton = GetComponent<Button>();
+
+        myButton.onClick.AddListener(() => {
+            myFunctionForOnClickEvent();
+
+        });
+
+    }
+
+    void myFunctionForOnClickEvent()
+    {
+        print("Bonsoir");
+        myButton.enabled = false;
+        myButton.gameObject.SetActive(false);
+    }
+}
