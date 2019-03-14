69351c2f3d51a0f05e3ecbe3d376c345abbab1ab
AlexandreCM <alexandre.mezeray@gmail.com>
2019-03-14 08:52:52 -0400

    Controlleur camera

---
+++ Niramos/Assets/Script/ControlleurCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ControlleurCamera : MonoBehaviour
+{
+    public Vector3 positionCamera = Vector3.zero;
+    public Transform pointVue;
+
+    void FixedUpdate()
+    {
+        positionCamera = new Vector3(
+            Mathf.SmoothStep(transform.position.x, pointVue.transform.position.x, 0.3f),
+            Mathf.SmoothStep(transform.position.y, pointVue.transform.position.y, 0.3f));
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = positionCamera + Vector3.forward * -10;
+    }
+}
