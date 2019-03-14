d7e9c6bc9c1d9a28a2a58fc625acf2d7341a3999
AlexandreCM <alexandre.mezeray@gmail.com>
2019-03-14 10:55:11 -0400

    Debut selection joueur

---
+++ Niramos/Assets/Script/SelectionJoueur.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionJoueur : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    void Awake()
+    {
+        //SelectJoueur1 = GetComponent<Button>().name = ;
+
+        /*myButton.onClick.AddListener(() => {
+            myFunctionForOnClickEvent();
+
+        });*/
+    }
+
+    public void btnClick()
+    {
+        player.GetComponent<mouvement>().enabled = true;
+        player.GetComponent<Rigidbody2D>().simulated = true;
+
+        /*print("C'est parti");
+        myButton.enabled = false;
+        myButton.gameObject.SetActive(false);
+        pointVue = GameObject.FindGameObjectWithTag("pointVueCamera").transform;
+        pointVue.position = new Vector3(0.0f, 3.0f, 0.0f);*/
+    }
+}
