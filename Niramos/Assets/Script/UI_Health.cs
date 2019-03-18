c55e6e8edb819c2a240bef4c4d0dd222d434fa69
FoxMcCloud45 <foxmccloud45@gmail.com>
2019-03-18 11:11:02 -0400

    Travail sur le HUD.

--- Niramos/Assets/Script/UI_Health.cs
+++ Niramos/Assets/Script/UI_Health.cs
@@ -7,25 +7,36 @@ public class UI_Health : MonoBehaviour
 {
 
     [SerializeField]
-    private Text vie_1;
-    [SerializeField]
-    private Text vie_2;
-    [SerializeField]
-    private Text vie_3;
-    [SerializeField]
-    private Text vie_4;
+    private Text vie1;
 
+[SerializeField]
+    private Text vie2;
     [SerializeField]
-    private VieJoueur joueur1;
+    private Text vie3;
     [SerializeField]
-    private VieJoueur joueur2;
-    [SerializeField]
-    private VieJoueur joueur3;
-    [SerializeField]
-    private VieJoueur joueur4;
+    private Text vie4;
+
+    /// <summary>
+    /// Tableau des labels.
+    /// </summary>
+    private Text[] labelsVie;
+
+    /// <summary>
+    /// Tableau des VieJoueur.
+    /// </summary>
+    private VieJoueur[] joueurs;
+
+    private int nombreJoueurs = 0;
+
     // Enabled
     private void OnEnable()
     {
+        this.joueurs = new VieJoueur[4];
+        this.labelsVie = new Text[4];
+        this.labelsVie[0] = this.vie1;
+        this.labelsVie[1] = this.vie2;
+        this.labelsVie[2] = this.vie3;
+        this.labelsVie[3] = this.vie4;
         GestionnaireEvenement.ajouterEvenement("vieChanger", vieJoueur);
     }
 
@@ -35,26 +46,41 @@ public class UI_Health : MonoBehaviour
     }
 
     private void vieJoueur(){
-
-        if (this.joueur1) {
-            if (this.joueur1.getVie() > 0) this.vie_1.text = this.joueur1.getVie().ToString();
-            else this.vie_1.text = "X";
+        if(this.nombreJoueurs <= 0) {
+            Debug.LogWarning("WARN    " + this.gameObject.name + ":UI_Health::vieJoueur(): Number of player is empty.");
         }
-        
-        if (this.joueur2) {
-            if (this.joueur2.getVie() > 0) this.vie_2.text = this.joueur2.getVie().ToString();
-            else this.vie_2.text = "X";
+        else {
+            for(int i = 0; i < this.nombreJoueurs; i++) {
+                if(this.joueurs[i] == null) {
+                    // Don't update.
+                }
+                else if(this.labelsVie[i] == null){
+                    Debug.LogWarning("WARN    " + this.gameObject.name + ":UI_Health::vieJoueur(): Missing label for given player.");
+                }
+                else {
+                    this.labelsVie[i].text = this.joueurs[i].getVie().ToString();
+                }
+            }
         }
+    }
 
-        if (this.joueur3) {
-            if (this.joueur3.getVie() > 0) this.vie_3.text = this.joueur3.getVie().ToString();
-            else this.vie_3.text = "X";
+    public void ajouterJoueur(VieJoueur v) {
+        if(this.nombreJoueurs >= 4) {
+            Debug.LogError("ERRR    " + this.gameObject.name + ":UI_Health::ajouterJoueur(" + v.gameObject.name + "): Number of players is above 4!!!");
         }
-
-        if (this.joueur4) {
-            if (this.joueur4.getVie() > 0) this.vie_4.text = this.joueur4.getVie().ToString();
-            else this.vie_4.text = "X";
+        else {
+            this.joueurs[this.nombreJoueurs] = v;
+            this.nombreJoueurs++;
         }
+    }
 
+    public void retirerDernierJoueur() {
+        if(this.nombreJoueurs <= 0) {
+            Debug.LogError("ERRR    " + this.gameObject.name + ":UI_Health::retirerDernierJoueur(): No player defined.");
+        }
+        else {
+            this.joueurs[this.nombreJoueurs] = null;
+            this.nombreJoueurs--;
+        }
     }
 }
