60e6b94fbf76970adffa8b57a9af3d745917aab8
FoxMcCloud45 <foxmccloud45@gmail.com>
2019-03-18 02:36:31 -0400

    Ajout de sprites de dégâts dans TestLevel_Multiplayer.

--- Niramos/Assets/Script/SpriteManager.cs
+++ Niramos/Assets/Script/SpriteManager.cs
@@ -7,10 +7,57 @@ using UnityEngine;
 /// </summary>
 public class SpriteManager : MonoBehaviour {
 
+    /// <summary>
+    /// La ressource à afficher en cas de réception de dommage.
+    /// </summary>
     [SerializeField]
     private GameObject damageEffect;
 
+    private bool damageEffectSet = false;
+
+    /// <summary>
+    /// Fonction exécutée quand le script est instancié.
+    /// Utilisée ici seulement à des fins de vérification.
+    /// </summary>
+    void onEnable() {
+
+        if(this.damageEffect == null) {
+            Debug.LogWarning("WARN    " + this.gameObject.name + ":SpriteManager::onEnable(): No damage sprite set; damage effects will not display.");
+        }
+        else {
+            // If the damage effect is set, we can show them!
+            this.damageEffectSet = true;
+        }
+    }
+
+    public static void creerEffetDegat(GameObject gfxsys, Vector3 position) {
+        if(gfxsys == null) {
+            Debug.LogWarning("WARN    SpriteManager::creerEffetDegat(Vector3 position): Referenced GameObject is NULL, discarded.");
+        }
+        else if (position == null) {
+            Debug.LogWarning("WARN    SpriteManager::creerEffetDegat(Vector3 position): No position given to spawn the effect to.");
+        }
+        SpriteManager spr = gfxsys.GetComponent<SpriteManager>();
+        if(spr == null) {
+            Debug.LogWarning("WARN    SpriteManager::creerEffetDegat(Vector3 position): Given GameObject has no SpriteManager!!!");
+        }
+        else if(spr.getDamageEffect() == null) {
+            Debug.LogWarning("WARN    SpriteManager::creerEffetDegat(Vector3 position): Requested SpriteManager from " + spr.gameObject.name + " has no damage sprite set.");
+        }
+        else {
+            spr.spawnDamageSprite(position);
+        }
+    }
+
     public void spawnDamageSprite(Vector3 position) {
         Instantiate(this.damageEffect, position, Quaternion.identity);
+    }
+
+    public bool isDamageEffectSet() {
+        return this.damageEffectSet;
+    }
+
+    public GameObject getDamageEffect() {
+        return this.damageEffect;
     }
 }
--- Niramos/Assets/Script/VieJoueur.cs
+++ Niramos/Assets/Script/VieJoueur.cs
@@ -8,13 +8,20 @@ public class VieJoueur : MonoBehaviour
     private float vie = 100;
     [SerializeField]
     private float vieMax = 100;
+
     [SerializeField]
     private bool isAlive;
     private bool estLocal;
 
+    [SerializeField]
+    private GameObject gfxManager;
+
     private void OnEnable()
     {
         this.setIfAlive(true);
+        if(this.gfxManager == null) {
+            Debug.LogWarning("WARN    " + this.gameObject.name + ":VieJoueur::OnEnable(): GFXManager GameObject not set; damage effects will not show for this player.");
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -25,6 +32,11 @@ public class VieJoueur : MonoBehaviour
         if (this.getIfAlive()) {
             vie -= quantiter;
             this.playDamageSound();
+            if(this.gfxManager) SpriteManager.creerEffetDegat(this.gfxManager, 
+            
+                                                              new Vector3(this.gameObject.transform.position.x,
+                                                                          this.gameObject.transform.position.y + 1,
+                                                                          this.gameObject.transform.position.z));
             if (vie <= 0 && this.estLocal) {
                 GestionnaireMort.getEvent().Invoke(this);
                 Debug.Log(this.gameObject.name + " est mort ; transmission au serveur.");
